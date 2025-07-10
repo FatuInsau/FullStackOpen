09cc8892a30c61c429c042fa4e97a872a5684ecc
InsauFatu <insaufaty@gmail.com>
2025-07-10 20:33:52 -0300

    avance

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1235567'
+     }
   ]) 
-  const [newName, setNewName] = useState('')
 
-  const addName = (event) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
     event.preventDefault()
     const repetido = persons.find((person) => { 
       return person.name === newName 
@@ -23,22 +27,26 @@ const App = () => {
     }
     
   }
-    
-
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value)
-    
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
