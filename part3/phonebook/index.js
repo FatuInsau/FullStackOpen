3d3ea400876fbbf4e2f4446d28e3bdd7dee4c15a
FatuInsau <insaufaty@gmail.com>
2025-07-23 22:10:43 -0300

    ejercicio 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+
 let persons = [
     { 
       "id": 1,
@@ -52,6 +55,34 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(p => p.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  
+  const body = request.body
+  
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
